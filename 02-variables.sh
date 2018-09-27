5bd7bfd2fc8f28d830459e1cd163f7bc7ab10d1b
Paul Beltran <beltran.pauljoseph@gmail.com>
2018-09-27 14:18:23 -0400

    Done variable

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+prenom=moi
+
+echo $prenom
+
+declare -i age=33 #varaible est un entier
+declare -r age2=33 #variable en lecture seule, impossible de reaffecter
+
+cours="Cour de Linux"
+
+#Variable systeme
+
+echo Prog $0 avec premier param $1, deuxieme param $2, total est $# param
+
+
